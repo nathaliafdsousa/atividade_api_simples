6d76e6e4f1d257d1f0ef9432676f37adbc3b8f08
nathaliafdsousa <nathalia.fdsousa@gmail.com>
2025-09-16 16:25:57 -0300

    Adiciona a documentação Swagger em todas as rotas da API FLASK

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flasgger import Swagger 
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 
 usuarios = []
@@ -8,6 +10,38 @@ contador_id = 1
 
 @app.route("/users", methods=["GET"])
 def listar_usuarios():
+    """Listar todos os usuários
+    
+    ---
+    tags:
+        - Usuarios
+    responses:
+        200:
+            description: lista de usuários
+            schema:
+                type:object
+                properties:
+                    mensagem:
+                        type: string
+                        example: Lista de usuários
+                    usuarios:
+                        type: array
+                        items:
+                            type: object
+                            properties:
+                                id:
+                                    type: integer
+                                    example: 1
+                                nome:
+                                    type: string
+                                    example: Rebeca
+                                email:
+                                    type: string
+                                    example: rebeca@email.com
+    
+    
+    """
+    
     if not usuarios:
         return jsonify({"mensagem": "Nenhum usuário cadastrado ainda", "usuarios": []}), 200
     return jsonify({"mensagem": "Lista de usuários", "usuarios": usuarios}), 200
@@ -15,6 +49,48 @@ def listar_usuarios():
 
 @app.route("/users/<int:user_id>", methods=["GET"])
 def buscar_usuario(user_id):
+    """Buscar usuários
+      ---
+    tags:
+        - Usuarios
+        parameters:
+            - name: user_id
+              in: path
+              type: integer
+              required: true
+              description: ID do usuário a ser buscado
+            responses:
+                200:
+                    description: Usuário encontrado
+                    scheme:
+                        type: object
+                        properties:
+                            mensagem:
+                                type: string
+                                example: Usuário encontrado
+                            usuario:
+                                type: object
+                                properties:
+                                    id:
+                                        type: integer
+                                        example: 1
+                                    nome:
+                                        type: string
+                                        example: Rebeca
+                                    email:
+                                        type: string
+                                        example: rebeca@email.com
+                404:
+                    description: Usuário não encontrado
+                    schema:
+                        type: object
+                        properties:
+                            erro:
+                                type: string
+                                example: Usuário não encontrado
+    
+    
+    """
     for usuario in usuarios:
         if usuario["id"] == user_id:
             return jsonify({"mensagem": "Usuário encontrado", "usuario": usuario}), 200
@@ -23,6 +99,59 @@ def buscar_usuario(user_id):
 
 @app.route("/users", methods=["POST"])
 def criar_usuario():
+    """Criar novos usuários
+        ---
+        tags:
+            - Usuários:
+            consumes:
+                - application/json
+                parameters:
+                    - int: body
+                        name: user
+                        required: true
+                        schema:
+                            type: object
+                            required: 
+                                - nome
+                                - email
+                            properties:
+                                nome:
+                                    type: string
+                                    example: Rebeca
+                                email:
+                                    type: string
+                                    example:rebeca@email.com
+            responses:
+                201:
+                    description:Usuário criado com sucesso
+                    schema:
+                        type: object
+                        properties:
+                            mensagem:
+                                type: string
+                                example: Usuário criado com sucesso
+                            usuario:
+                                type: object
+                                properties:
+                                    id:
+                                        type: integer
+                                        example: 1
+                                    nome:
+                                        type: string
+                                        example: Rebeca
+                                    email:
+                                        type: string
+                                        example: rebeca@email.com
+                400:
+                    description: Requisição inválida
+
+
+    
+    
+    
+    """
+    
+    
     global proximo_id
     dados = request.get_json()
 
@@ -42,6 +171,41 @@ def criar_usuario():
 
 @app.route("/users/<int:user_id>", methods=["PUT"])
 def atualizar_usuario(user_id):
+    """Atualizar usuários
+    ---
+    tags:
+        - Usuarios
+        consumes:
+            - application/json
+            parameters:
+                - name: user_id
+                  in: path
+                  type: integer
+                  required: true
+                  description: ID do usuário a ser atualizado
+                - in: body
+                  name: user
+                  required: true
+                  schema:
+                    type: object
+                    properties:
+                        nome:
+                            type: string
+                            example: Rebeca
+                        email:
+                            type: string
+                            example:rebeca@email.com
+        responses:
+            200:
+            description: Usuário atualizado com sucesso
+            400:
+                description: Requisição inválida
+            404:
+                description: Usuário não encontrado
+
+
+    """
+    
     dados = request.get_json()
 
     if not dados:
@@ -58,6 +222,41 @@ def atualizar_usuario(user_id):
 
 @app.route("/users/<int:user_id>", methods=["DELETE"])
 def deletar_usuario(user_id):
+    """Deletar usuários
+    ---
+    tags:
+        - Usuarios
+        parameters:
+            - name: user_id
+              in: path
+              type: integer
+              required: true
+              description: ID do usuário a ser deletado
+            responses:
+                200:
+                    description: Usuário deletado com sucesso
+                    schema:
+                        type: object
+                        properties:
+                            mensagem:
+                                type: string
+                                example: Usuário deletado com sucesso
+                404:
+                    description: Usuário não encontrado
+                    schema:
+                        type: object
+                        properties:
+                            erro:
+                                type: string
+                                example: Usuário não encontrado
+    
+    
+    """
+    
+    
+    
+    
+    
     for i, usuario in enumerate(usuarios):
         if usuario["id"] == user_id:
             usuarios.pop(i)
